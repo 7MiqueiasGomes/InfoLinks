4bf595d53638d9bb5c9fa933b9e61a08f3756097
Miqueias <l0rdz3us0@email.com>
2025-04-05 17:15:31 -0300

    Adicionando Estilização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function index () {
 return (
 <View>
-    <Text>eu sou o pior!!</Text>
-    <Text>Miquéias</Text>
+    <Text style={styles.title} >eu sou o melhor!!</Text>
+    <Text style={styles.title2} >Miquéias</Text>
+    <Text style={styles.title3} >Nascimento: 05/07</Text>
+    <Text style={styles.title4} >Rua tal</Text>
 </View>
 );
 }
+
+const styles = StyleSheet.create({
+    title: {
+        color: "red",
+        fontSize: 22,
+    },
+
+    title2: {
+        color: "blue",
+        fontSize: 26,
+    },
+
+    title3: {
+        color: "green",
+        fontSize: 24,
+    },
+
+    title4: {
+        color: "orange",
+        fontSize: 24,
+    },
+
+
+})
+
+
+
+
+

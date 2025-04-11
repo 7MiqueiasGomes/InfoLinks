aefa5006b7aef060f9d3661375b4e55117916361
Miqueias <l0rdz3us0@email.com>
2025-04-11 20:47:28 -0300

    InfoLinks

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,12 @@
+// importando componentes de dentro do react native
 import { View, Text, StyleSheet } from "react-native";
 
+// Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function index () {
 return (
-<View>
+
+// View é um componente que recebe estilização, semelhante a Div do HTML
+<View style={styles.container}>
     <Text style={styles.title} >eu sou o melhor!!</Text>
     <Text style={styles.title2} >Miquéias</Text>
     <Text style={styles.title3} >Nascimento: 05/07</Text>
@@ -11,7 +15,16 @@ return (
 );
 }
 
+// estilizações criadas usando o stylesheet
 const styles = StyleSheet.create({
+    
+    container: {
+        flex: 1, // considera toda a arca util da tela para os componentes
+        justifyContent: "center", // justifica (ajusta) todos os elementos no centro da tela 
+        alignItems: "center", // alinha todos os elementos no centro da tela
+        flexDirection: "row", // organiza todos os elementos dentro do flex box em linha (esquerda)
+    },
+
     title: {
         color: "red",
         fontSize: 22,
